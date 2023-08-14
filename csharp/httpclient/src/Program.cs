c90aa1104af778e704d35712ef870f6d0b1b1404
ni-sgn <saganeli.nika@gmail.com>
2023-08-15 01:27:40 +0400

    initial commit

---
+++ csharp/httpclient/src/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+namespace test;
+
+
+// learn streams in c#
+
+public class EntryPoint{
+    public static async Task<string> GetData()
+    {
+        HttpClient client = new HttpClient();
+        //client.BaseAddress = new Uri("google.com"); 
+
+        var data = await client.GetAsync("https://google.com");
+        if (data.IsSuccessStatusCode)
+            return await (data?.Content?.ReadAsStringAsync() ?? throw new ArgumentException("This shit is null"));
+        else throw new Exception("Could not read the data");
+    }
+
+    public static async Task Main()
+    {
+        try{
+            var result = await GetData();
+            WriteLine(result);
+        }
+        catch(Exception ex)
+        {
+            WriteLine(ex.Message);
+        }
+    }
+
+}
