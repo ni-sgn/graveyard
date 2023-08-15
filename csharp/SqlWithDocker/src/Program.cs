2fc1ae6bf957b8b4be76b2865d2a4986b4915c78
ni-sgn <saganeli.nika@gmail.com>
2023-08-15 18:43:52 +0400

    some bs from project, idk why gitignore is not working

---
+++ csharp/SqlWithDocker/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dapper;
+using System.Collections.Generic;
+
+namespace program;
+
+public class EntryPoint{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
