f38ae3d19eecdb8d9f0d36f1d8668c714fafebff
Nika <nika.saganelidze.1@gmail.com>
2023-08-17 15:13:18 +0400

    datetime stuff (#1)
    * datetime stuff
    
    * modified some unnecessary stuff
    
    ---------
    
    Co-authored-by: nika.saganelidze <nika.saganelidze@basisbank.ge>
    Co-authored-by: ni-sgn <saganeli.nika@gmail.com>

---
+++ csharp/DateTime/src/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace date;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"{GetAggregatedCashbacksDateRange().Item1}, {GetAggregatedCashbacksDateRange().Item2}");
+    }
+
+    public static (DateTime, DateTime) GetAggregatedCashbacksDateRange() { 
+       var today = DateTime.Today;
+       var month = new DateTime(today.Year, today.Month, 1);
+       var first = month.AddMonths(-1);
+       var last = month.AddDays(-1);
+       return (first, last);
+    }
+
+}
+
