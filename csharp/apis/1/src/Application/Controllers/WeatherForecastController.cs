49209b9aa94cb0e5e3a92d9e527a9484ae0ebe78
ni-sgn <saganeli.nika@gmail.com>
2023-09-22 21:42:49 +0400

    implemented person creation

--- csharp/apis/1/src/Application/Controllers/WeatherForecastController.cs
+++ csharp/apis/1/src/Application/Controllers/WeatherForecastController.cs
@@ -1,20 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using business;
+using Microsoft.EntityFrameworkCore;
+using DAL;
 
 namespace api.Controllers;
 
 [ApiController]
 [ApiVersion("1", Deprecated = false)]
 [Route("api/v{version:apiVersion}/[controller]/[action]")]
-public class WeatherForecastController : ControllerBase
+public class MiscController : ControllerBase
 {
     private static readonly string[] Summaries = new[] {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<MiscController> _logger;
+    private readonly SocialLifeContext _context;
+    private readonly PeopleOperations _po; 
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+    public  MiscController(
+      ILogger<MiscController> logger,
+      SocialLifeContext context 
+      ) {
         _logger = logger;
+        _context = context;
+        PeopleOperations po = new PeopleOperations(_context);
+        _po = po;
     }
 
     /// <summary>
@@ -31,4 +42,16 @@ public class WeatherForecastController : ControllerBase
     public string Get2(){
       return "something else";
     }
+
+    [HttpPost(Name = "AddPerson")]
+    public ActionResult AddPerson(string firstname, string lastname, DateTime birthDate) {
+      try {
+        _po.CreatePerson(new DAL.entities.Person{first_name = firstname, last_name = lastname, date_of_birth = birthDate});
+        return StatusCode(200);
+      } catch (Exception ex) {
+        _logger.LogError(ex, "something went wrong");
+        return StatusCode(400); //what's 400 btw?
+      }
+    }
+
 }
