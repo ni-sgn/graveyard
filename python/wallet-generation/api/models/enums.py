6a16379bed57bdd6f871332b620271548cabf98d
nika <nika@pop-os.localdomain>
2023-12-06 23:26:34 +0400

    generating but not really

---
+++ python/wallet-generation/api/models/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from hdwallet import symbols
+
+class SymbolEnum(str, Enum):
+    ETH = symbols.ETH
+    BTC = symbols.BTC
+
+class StrengthEnum(int, Enum):
+    S128 : int = 128
+    S160 : int = 160
+    S192 : int = 192
+    S224 : int = 224
+    S256 : int = 256
+
+class LanguageEnum(str, Enum):
+    EN : str = 'english'
---
+++ python/wallet-generation/api/wallet_facade.py
@@ -0,0 +1,25 @@
+import hdwallet
+import hdwallet.utils
+import hdwallet.symbols
+import typing
+
+import json
+
+
+class HDWalletFacade():
+    def __init__(self, symbol : str, strength : int, language : str, passphrase : str, index : int, hardened : bool):
+        self._symbol : str = symbol
+        self._strength : int = strength
+        self._language : str = language 
+        self._passphrase : typing.Optional[str] = passphrase
+        self._index : int = index
+        self._hardened : bool = hardened
+
+        self._entropy : str = hdwallet.utils.generate_entropy(strength=self._strength) 
+        self._hdwallet = hdwallet.HDWallet(symbol=self._symbol)
+        self._hdwallet.from_entropy(entropy=self._entropy, language=self._language, passphrase=self._passphrase)
+
+    def derive_wallet(self):
+        self._hdwallet.from_index(44, hardened=self._hardened)
+        return self._hdwallet.dumps()
+    
